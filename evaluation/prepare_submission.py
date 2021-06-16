3c5617539a0acf26d431a6def1195d8640d5176c
Rami Aly <rami-aly@hotmail.dewq>
2021-06-16 16:17:29 +0100

    prepare submission file

--- evaluation/prepare_submission.py
+++ evaluation/prepare_submission.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_file', type=str)
-    parser.add_argument('--split', type=str)
     args = parser.parse_args()
 
     predictions = []
@@ -18,12 +17,11 @@ if __name__ == "__main__":
                 continue
             curr_l = {}
             curr_l['id'] = line['id']
-            curr_l['predicted_label'] = line['predicted_verdict']
+            curr_l['predicted_label'] = line['predicted_label']
             curr_l['predicted_evidence'] = [[el.split('_')[0], el.split('_')[1] if 'table_caption' not in el and 'header_cell' not in el else '_'.join(el.split('_')[1:3]), '_'.join(el.split('_')[2:]) if 'table_caption' not in el and 'header_cell' not in el else '_'.join(el.split('_')[3:])] for el in  line['predicted_evidence']]
             predictions.append(curr_l)
 
 
-
-    with jsonlines.open('submission_{}.jsonl'.format(args.split), 'w') as f:
+    with jsonlines.open('{}.jsonl'.format(args.input_file.split('.jsonl')[0] + '.submission'), 'w') as f:
         for ele in predictions:
             f.write(ele)
