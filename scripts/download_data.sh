6493f708849177c82c427ac864d88ef016bbdab8
Rami <rami-aly@hotmail.de>
2023-02-16 20:07:03 +0000

    Update download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,7 +1,7 @@
 mkdir -p data
 mkdir -p models
-wget -O data/train.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/feverous/train.jsonl
-wget -O data/dev.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/feverous/dev.jsonl
-# wget -O data/test.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_test.jsonl
-wget -O data/feverous-wiki-pages-db.zip https://s3-eu-west-1.amazonaws.com/fever.public/feverous/feverous-wiki-pages-db.zip
+wget -O data/train.jsonl https://fever.ai/download/feverous/feverous_train_challenges.jsonl
+wget -O data/dev.jsonl https://fever.ai/download/feverous/feverous_dev_challenges.jsonl
+wget -O data/test_unlabeled.jsonl https://fever.ai/download/feverous/feverous_test_unlabeled.jsonl
+wget -O data/feverous-wiki-pages-db.zip https://fever.ai/download/feverous/feverous-wiki-pages-db.zip
 unzip data/feverous-wiki-pages-db.zip
