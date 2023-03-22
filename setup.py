27530db4afe847a9ac3dd0ed792542490fea521b
Rami Aly <rmya2@cl.cam.ac.uk>
2023-03-22 02:10:12 +0000

    small fixes and updated requirements.txt

--- setup.py
+++ setup.py
@@ -9,16 +9,24 @@ HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
-print(HERE)
 
-# Dependencies required to use your package
-with open(os.path.join("src", "feverous", "requirements.txt"), 'r') as fh:
-    INSTALL_REQS = [l.strip() for l in fh.readlines() if l.strip()]
-
+# Dependencies required to use
+INSTALL_REQS = [
+    "regex>=2020.10.23, <= 2022.10.31",
+    "numpy>=1.19.1, <= 1.21.6",
+    "transformers>=4.1.1, <= 4.26.1",
+    "scipy>=1.5.4, <= 1.7.3",
+    "clean-text>=0.4.0, <= 0.6.0",
+    "pexpect<=4.8.0",
+    "spacy>=2.3.2, <= 3.5.0",
+    "scikit-learn<=1.0.2",
+    "jsonlines>=2.0.0, <= 3.1.0",
+    "tqdm>=4.46.0, <= 4.64.1",
+]
 
 setup(
-    name='feverous',
-    version='0.5',
+    name="feverous",
+    version="0.51",
     description="Repository for Fact Extraction and VERification Over Unstructured and Structured information (FEVEROUS), used for the FEVER Workshop Shared Task at EMNLP2021.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -26,8 +34,8 @@ setup(
     author="Rami Aly",
     author_email="rmya2@cam.ac.uk",
     license="MIT",
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=INSTALL_REQS,
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
